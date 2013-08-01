4e71cdc010b2ff7f05443c6290cf80cc03f48b50
Mitko Nikolov <mitko.nikoloff@gmail.com>
2013-08-01 23:55:23 +0300

    Revert "Homework assignments"
    This reverts commit 226b799e333a4f3a5e386e7f73f8da0de1af8ce1.

--- 01. IntroductionToPrograming/3. MyName/MyName.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-class MyName
-{
-    static void Main()
-    {
-        Console.WriteLine("Name should be here");
-    }
-}
--- 01. IntroductionToPrograming/4. 1, 101, 1001/Print_1_101_1001.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-class Print_1_101_1001
-{
-    static void Main()
-    {
-        Console.WriteLine("{0}\n{1}\n{2}", 1, 101, 1001);
-    }
-}
